de4952e46478f913cf33ad1a17def179345aca98
pablorenan-dev <pablonichterwistz@outlook.com>
2026-06-27 15:58:46 -0300

    deploy: frontend para gh-pages

--- backend/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { login, registro } from '../controllers/authController.js';
-
-const router = express.Router();
-
-router.post('/login',    login);
-router.post('/registro', registro);
-
-export default router;
--- backend/src/server.js
+++
@@ -1,8 +0,0 @@
-import app from '../app.js';
-import 'dotenv/config';
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
